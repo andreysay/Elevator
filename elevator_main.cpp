c3537bcbc68c463623aa28c4ba0d21eaf2b1e893
Andrey Spitsyn <andreysay@live.com>
2017-09-11 11:48:04 +0300

    Update elevator_main.cpp

--- elevator_main.cpp
+++ elevator_main.cpp
@@ -1,5 +1,11 @@
-//Title of this code
+// ***** 0. Documentation Section *****
+// elevator_main.cpp
+// Runs on Linux
+// The program emulates the movement of the elevator. 
+// Andrey Spitsyn
+// September 10, 2017
 
+// ***** 1. Pre-processor Directives Section *****
 #include <iostream>
 #include <ctime>
 #include <list>
@@ -12,18 +18,17 @@
 #include <unistd.h>
 #include <limits.h>
 
-#define INPUT_LIMIT 3
-#define FLOORS_LIMIT 100
+#define INPUT_LIMIT 3	 // max three digit decimal number
+#define FLOORS_LIMIT 100 // suppose there is no buildings above FLOORS_LIMIT
 
-using namespace std;
+using namespace std; // just to siplify code
 
-enum class elev_direction { UP, DOWN, IDLE };
-
-void enable_raw_mode()
+// ***** 2. Subroutines Section *****
+void enable_raw_mode() // Disable echo as well
 {
     termios term;
     tcgetattr(0, &term);
-    term.c_lflag &= ~(ICANON | ECHO); // Disable echo as well
+    term.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(0, TCSANOW, &term);
 }
 
@@ -35,21 +40,23 @@ void disable_raw_mode()
     tcsetattr(0, TCSANOW, &term);
 }
 
-bool kbhit()
+bool kbhit() // detect key press
 {
     int byteswaiting;
     ioctl(0, FIONREAD, &byteswaiting);
     return byteswaiting > 0;
 }
 
+// ***** 2. Classes Section *****
+// Elevetor class
 class Elevator {
-    int elevatorLevel;
-    elev_direction elev_move;
-    set<int> levelsToReach;
+    enum class elev_direction { UP, DOWN, IDLE };
+    int elevatorLevel; // Elevator current level
+    elev_direction elev_move; // Elevator current direction
+    set<int> levelsToReach; // Set of levels to reach
 
   public:
-
-    Elevator() = delete;
+    Elevator() = delete; // no constructor without parameters
     Elevator(int level) : elevatorLevel{level}, elev_move{elev_direction::IDLE} {}
 
     int getCurrentLevel() {
@@ -57,7 +64,6 @@ class Elevator {
     }
 
     elev_direction getDirection() {
-        //  -1,0,1
         return elev_move;
     }
 
@@ -66,7 +72,6 @@ class Elevator {
     }
 
     bool isDirectionGood(int level_req) {
-        // depends on requester level and requested level
     	bool ret_value = false;
     	if( level_req <  elevatorLevel  && elev_move == elev_direction::DOWN ){
     		ret_value = true;
@@ -80,46 +85,37 @@ class Elevator {
     }
 
     void addNewRequest(int level_req) {
-
-        // not good need to be ordered , no duplicates
     	if( level_req < elevatorLevel ){
     		setDirection(elev_direction::DOWN);
     	} else {
     		setDirection(elev_direction::UP);
     	}
-
         levelsToReach.insert(level_req);
     }
 
-    bool move() {
+    bool move() { // emulate elevator movement
     	bool ret_value = false;
-//    	cout << "Enter move\nLevels to reach is: " << levelsToReach.size() << endl;
-        if ( !levelsToReach.empty() ) {
+        if ( !levelsToReach.empty() ) { // if current set of requests is not empty move elevator
             auto level_to_reach = levelsToReach.begin();
-//            cout << "Level to reach: " << *level_to_reach << endl;
-            if( elevatorLevel == *level_to_reach ){
+            if( elevatorLevel == *level_to_reach ){ // if elevator reach max floor request
             	cout << "Cabin on floor " << elevatorLevel << " Stopped. Doors opened." << endl;
-//            	cout << "Equal range for " << elevatorLevel << " " << *levelsToReach.equal_range(elevatorLevel).first << endl;
             	levelsToReach.erase(elevatorLevel);
-//            	setDirection(elev_direction::IDLE);
             	ret_value = true;
-            } else if( elevatorLevel == *levelsToReach.lower_bound(elevatorLevel) ){
+            } else if( elevatorLevel == *levelsToReach.lower_bound(elevatorLevel) ){ // if elevator reach concomitant level
             	cout << "Cabin on floor " << elevatorLevel << " Stopped. Doors opened." << endl;
             	levelsToReach.erase( elevatorLevel );
             	ret_value = true;
             }
             if ( elevatorLevel < *level_to_reach ){
             	cout << "Cabin on floor " << this->getCurrentLevel() << " Going UP!" << endl;
-//            	cout << "Lower bound: " << *levelsToReach.lower_bound(elevatorLevel) << endl;
             	++elevatorLevel;
             } else if(elevatorLevel > *level_to_reach){
             	cout << "Cabin on floor " << this->getCurrentLevel() << " Going DOWN!" << endl;
-//            	cout << "Lower bound: " << *levelsToReach.lower_bound(elevatorLevel) << endl;
             	--elevatorLevel;
             } else {
 
             }
-        } else {
+        } else { // all requests handled
         	cout << "Cabin on floor " << elevatorLevel << " Stopped. Doors opened." << endl;
         	setDirection( elev_direction::IDLE );
         	ret_value = false;
@@ -127,69 +123,62 @@ class Elevator {
         return ret_value;
     }
 
-};
+}; // Class Elevator
 
 
-
+// Elevator control class
 class ElevatorControl {
-
-    Elevator elevator{1};
-    set<int> requests;
-    int numLevels;
-
+    Elevator elevator{1}; // elevator initialisation
+    set<int> requests; // set to store requests from floors
+    int numLevels; // max num floors in the building
   public:
-
     ElevatorControl(int maxLevel) : numLevels{maxLevel} {}
 
-    bool step(){
+    bool step(){ // handle requests and move elevator
     	set<int> temp;
- //   	cout << "Enter step!" << endl;
         for (auto level_to_reach : requests) {
- //       	cout << level_to_reach << endl;
-        	if( elevator.isDirectionGood(level_to_reach) ){
+        	if( elevator.isDirectionGood(level_to_reach) ){ // if request from concomitant level handle it
         		elevator.addNewRequest(level_to_reach);
         		temp.insert(level_to_reach);
             }
         }
-        for (auto level_to_reach : temp) {
+        for (auto level_to_reach : temp) { // remove requests which will handle by elevator 
         	requests.erase(level_to_reach);
         }
-        // here do something smart thing with elevators
+        // here move elevator
         return elevator.move();
     }
 
-
-    void elevCall(int floor) {
+    void elevCall(int floor) { // call elevator
         requests.insert(floor);
     }
 
-    bool isThereElevCall() {
+    bool isThereElevCall() { // check if request set is not empty
     	return !requests.empty();
     }
 
 
-    void printStatus() {
-
+    void printStatus() { // help function to visualise elevator movement
         for (int level = numLevels; level >= 1; --level) {
         	if (elevator.getCurrentLevel() == level)
         		cout << "#";
-            else
-            	cout << ".";
+        	else
+            		cout << ".";
         }
         cout << endl;
     }
 
-};
+}; // Class Elevator control
 
 
 
 int main()
 {
-	char input[INPUT_LIMIT];
+	char input[INPUT_LIMIT]; // store user input
 	uint32_t max_floors_num = 0;
-    uint32_t request_from_floor = 0;
+    	uint32_t request_from_floor = 0;
 	cout << "To close the program type \"CTRL+c\"\nPlease enter MAX number of floors in your building <= " << FLOORS_LIMIT << " : " << endl;
-	while ( cin.getline(input, INPUT_LIMIT ) ){
+	while ( cin.getline(input, INPUT_LIMIT ) ){ // get max number floors, suppose there is no buildings above FLOORS_LIMIT
 		if( isdigit(input[0]) ){
 			max_floors_num = atoi(input);
 			break;
@@ -199,11 +188,11 @@ int main()
 		}
 
 	}
-    if ( max_floors_num < 1 || max_floors_num > FLOORS_LIMIT ){
-    	cout << "ENTERED WRONG FLOORS NUMBER!!!" << endl;
-    	return 0;
-    }
-    cout << "Enter floor which you want to reach : " << endl;
+    	if ( max_floors_num < 1 || max_floors_num > FLOORS_LIMIT ){ // if max_floors above limit end program
+    		cout << "ENTERED WRONG FLOORS NUMBER!!!" << endl;
+    		return 0;
+    	}
+    	cout << "Enter floor which you want to reach : " << endl;
 	while ( cin.get(input, INPUT_LIMIT) ){
 		if( isdigit(input[0]) ){
 			request_from_floor = atoi(input);
@@ -213,10 +202,10 @@ int main()
 			cout << "Enter floor which you want to reach:" << endl;
 		}
 	}
-    while( request_from_floor < 1 || request_from_floor > max_floors_num ){
-    	cout << "Please enter right floor number: " << endl;
-    	while ( cin.getline(input, INPUT_LIMIT ) ){
-    		if( isdigit(input[0]) ){
+    	while( request_from_floor < 1 || request_from_floor > max_floors_num ){ // check requested floor
+    		cout << "Please enter right floor number: " << endl;
+    		while ( cin.getline(input, INPUT_LIMIT ) ){
+    			if( isdigit(input[0]) ){
     			request_from_floor = atoi(input);
     			break;
     		} else {
@@ -226,34 +215,34 @@ int main()
 
     	}
     }
-	ElevatorControl ec(max_floors_num);
+    // Create elevator control object
+    ElevatorControl ec(max_floors_num);
+    // add request from a floor
     ec.elevCall(request_from_floor);
+    // main loop for elevator movement
     while( 1 ){
-    	ec.printStatus();
+    	ec.printStatus(); // visualise elevator movement 
     	enable_raw_mode();
-    	while( !kbhit() && !ec.step() ){
+    	while( !kbhit() && !ec.step() ){ // if no keyboard press or elevator reach floor
     		sleep(1);
     		cout << "<Press any key to enter new command>";
     		ec.printStatus();
     	}
     	disable_raw_mode();
     	tcflush(0, TCIFLUSH); // Clear stdin to prevent characters appearing on prompt
-    	if( cin.get(input, INPUT_LIMIT) && isdigit(input[0]) ){
+    	if( cin.get(input, INPUT_LIMIT) && isdigit(input[0]) ){ // when elevator reach a floor ask for new request
     		request_from_floor = atoi(input);
-//    		cout << "Got floor number!" << endl;
-    	} else {
+    	} else { // if something wrong with input, clear input stream and continue
     		request_from_floor = 0;
-//    		cout << "input: " << input << " cin cleared!" << endl;
     		cin.clear();
     		cin.ignore(100, '\n');
     		if( cin.get(input, INPUT_LIMIT) && isdigit(input[0]) ){
     			request_from_floor = atoi(input);
-//    		    cout << "Got floor number!" << endl;
     		}
     	}
-        if( request_from_floor >= 1 && request_from_floor <= max_floors_num ){
+        if( request_from_floor >= 1 && request_from_floor <= max_floors_num ){ // check floor number and add to elevator call set
         	ec.elevCall(request_from_floor);
-        } else {
+        } else { // continue
         	request_from_floor = 0;
         }
     }
